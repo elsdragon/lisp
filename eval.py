fb131fe7a568ac7001b375b5cf03d4b7ea1f017c
Lucia <la_els@hotmail.com>
2023-11-16 00:10:21 +0100

    Creacion del bucle while infinito para solicitar datos y capturar SyntaxError desde aqui. Eliminacion en read de la captura

--- eval.py
+++ eval.py
@@ -1,5 +1,10 @@
+from enum import Enum
 
 def eval(lisp):
+
+    ''' 
+    Calculate floor from a string with a LISP Code. 
+    '''
 
     # Lee la cadena de caracteres
     characters = read(lisp)
@@ -7,6 +12,7 @@ def eval(lisp):
     # Calcular el piso
     floor = 0
     for char in characters:
+        
         if char == "(":
             floor += 1
         elif char == ")":
@@ -26,12 +32,12 @@ def read(string):
     new_string = ""
     # comprueba que la string pasada as válida
     # Captura el error SyntaxisError y devuelve un mensaje del mismo
-    try:
-        if check(string):
-        # si es valida limpia la string de spacios en blancos
-            new_string = parse(string)
-    except SyntaxError as error:
-        print(f'Your string is not valid: {error}')
+    #try:
+    if check(string):
+    # si es valida limpia la string de spacios en blancos
+        new_string = parse(string)
+    #except SyntaxError as error:
+        #print(f'Your string is not valid: {error}')
 
     
     
@@ -65,4 +71,40 @@ def check(string):
     return valid_string
 
 
+
+
+def eval_rpl():
+    ''' 
+    Infinte while bucle.
+    User introduce a LISP Code as string or quit. 
+    When raise a SyntaxErro return a massage. 
+    '''
+
+
+    class UserChoice(Enum):
+        LISP = 0
+        QUIT = 1
+
+    while True:
+        print(f"{UserChoice.LISP.value}. I want add a LISP CODE")
+        print(f'{UserChoice.QUIT.value}. I want exit')
+        print("***************************************")
+        answer = int(input('Enter your option: '))
+
+        if answer == 0:
+            new_string = input("Introduce your LISP code: ")
+
+            try: 
+                floor = eval(new_string)
+                print(f"your floor is {floor}\n")
+            except SyntaxError as err:
+                print(f'{err}\n')
+        else:
+            break
+        
+
+    pass
+
+if __name__== "__main__":
+    eval_rpl()
 
